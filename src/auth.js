f83104508f4bcc03643f94789b034d07a89b30e9
Karan Khot <karankhot@Karans-MacBook-Air.local>
2024-06-09 22:14:09 -0500

    all0609

--- src/auth.js
+++ src/auth.js
@@ -6,8 +6,8 @@ import User from './models/User';
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
   callbacks: {
