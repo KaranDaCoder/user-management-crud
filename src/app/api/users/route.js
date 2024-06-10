f82ce54856f8f3e8b208f0dba763b12b32084ac1
Karan Khot <karankhot@Karans-MacBook-Air.local>
2024-06-09 23:46:24 -0500

    all0609-users-api-dynamic

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,20 +1,21 @@
-import { connectDb } from "@/lib/dbConnect"
-import { auth } from "../../../auth";
-import User from "@/models/User";
-import { NextResponse } from "next/server";
+export const dynamic = 'force-dynamic';
+import { connectDb } from '@/lib/dbConnect';
+import { auth } from '../../../auth';
+import User from '@/models/User';
+import { NextResponse } from 'next/server';
 
-export const GET = async(request, {params}) => {
- try {
-  await connectDb();
-  const session = await auth();
-  console.log(`SESSION IN API : ${JSON.stringify(session)}`)
-  const all_users = await User.find({});
-  return new NextResponse(JSON.stringify(all_users), {status:200})
+export const GET = async (request, { params }) => {
+ const session = await auth();
+  try {
+    await connectDb();
+    console.log(`SESSION IN API : ${JSON.stringify(session)}`);
+    const all_users = await User.find({});
+    return new NextResponse(JSON.stringify(all_users), { status: 200 });
   } catch (error) {
-   console.log(error)
-   return new NextResponse(JSON.stringify(error), {status:200})
- }
-}
+    console.log(error);
+    return new NextResponse(JSON.stringify(error), { status: 200 });
+  }
+};
 
 // export const GET = auth(async function GET(request, {params}) {
 //  try {
